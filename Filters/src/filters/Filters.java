cc99d07a66f49f6922878ccb2f7a7497bd05c779
omgKyleyay <1337scriptdoggy@gmail.com>
2018-04-28 23:48:52 -0400

    Filters Image
    Includes blur, sharpen, emboss, edge, and mean filters. The gauss does not work yet.

---
+++ Filters/src/filters/Filters.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+/**
+ *
+ * @author Kyle
+ */
+public class Filters {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FilterFrame f = new FilterFrame();
+        f.setVisible(true);
+    }
+    
+}
